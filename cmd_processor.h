76a74eda671f04208e7b1ab9d1a2a1d9baffddd8
smallmall <138298455+smallmall@users.noreply.github.com>
2023-07-10 19:29:29 +0800

    Update cmd_processor.h
    改掉了const

--- cmd_processor.h
+++ cmd_processor.h
@@ -27,13 +27,13 @@ class CmdProcessor {
 bool AddTaskNoOp(TaskList& task_list);
 bool ModifyTaskNoOp(TaskList& task_list);
 bool DeleteTaskNoOp(TaskList& task_list);
-bool ShowTaskNoOp(const TaskList& task_list);
-bool SearchTaskNoOp(const TaskList& task_list);
+bool ShowTaskNoOp(TaskList& task_list);
+bool SearchTaskNoOp(TaskList& task_list);
 bool should_quit(string choice);
 
 // All ops(options) start with "-" and have only one character.
 bool AddTaskOp(TaskList& task_list, list<string> cmd);  // ops: "n:b:r:pt"
 bool ModifyTaskOp(TaskList& task_list, list<string> cmd);  // "i:n:C:T" i--id, n--name, C--Change, T--To. id 和 name 有一个即可。C 后接要改变的东西，如nbrpt(name,begin_time...), 不论是简写还是完整的都要支持。T 后接变成的内容，注意要检查。
 bool DeleteTaskOp(TaskList& task_list, list<string> cmd);   // "i:n:a" a--all.
-bool ShowTaskOp(const TaskList& task_list, list<string> cmd);  // "S:E:p:t:A:D:" S--Start, E--End, A--Ascending, D--Descending. A 和 D 后接inbrp(id, name, begin_time, remind_time, priority), 用于排序。
-bool SearchTaskOp(const TaskList& task_list, list<string> cmd);  // "i:n:"
+bool ShowTaskOp(TaskList& task_list, list<string> cmd);  // "S:E:p:t:A:D:" S--Start, E--End, A--Ascending, D--Descending. A 和 D 后接inbrp(id, name, begin_time, remind_time, priority), 用于排序。
+bool SearchTaskOp(TaskList& task_list, list<string> cmd);  // "i:n:"
